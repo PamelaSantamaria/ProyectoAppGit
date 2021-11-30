7b9d6bec3471d727f2cc706c45716d61fc629c7c
Pamela Santamaría <78579909+PamelaSantamaria@users.noreply.github.com>
2021-11-30 11:05:07 -0600

    Update gameGit.py

--- Entregable1/gameGit.py
+++ Entregable1/gameGit.py
@@ -28,7 +28,9 @@ while(unstod!=0):
 os.system("cls")
 
 print("\nOk, " + name + " a todo esto, ¿qué es Git?\n")
-print('''Git es un sistema de control de versiones distribuido. Esto significa que un clon local del proyecto es un repositorio de control de versiones completo. Estos repositorios locales plenamente funcionales permiten trabajar sin conexión o de forma remota fácilmente. Los desarrolladores confirman su trabajo localmente y, a continuación, sincronizan su copia del repositorio con la copia en el servidor.''')
+print('''Git es un sistema de control de versiones distribuido. Esto significa que un clon local del proyecto es un repositorio de control de versiones 
+        completo. Estos repositorios locales plenamente funcionales permiten trabajar sin conexión o de forma remota fácilmente. Los desarrolladores 
+        confirman su trabajo localmente y, a continuación, sincronizan su copia del repositorio con la copia en el servidor.''')
 
 time.sleep(10)
 print("\nTenemos una prueba para ti, estás listx, " + name + "?")
