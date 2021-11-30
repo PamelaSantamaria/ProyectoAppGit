9a589ff7128b2f5f9ae8c4a42be96c094675715c
Pamela Santamaría <78579909+PamelaSantamaria@users.noreply.github.com>
2021-11-30 11:00:50 -0600

    Cambios generales
    Separada la clase Question y se añadió una trivia

---
+++ Entregable1/Question.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 30 10:02:40 2021
+
+@author: PamSH
+"""
+
+class Question:
+    def __init__(self, prompt, answer):
+        self.prompt= prompt
+        self.answer= answer
--- Entregable1/gameGit.py
+++ Entregable1/gameGit.py
@@ -6,10 +6,7 @@ Created on Sun Nov 28 21:02:28 2021
 """
 import time
 import os
-class Question:
-    def __init__(self, prompt, answer):
-        self.prompt= prompt
-        self.answer= answer
+from Question import Question
 
 print("Bienvenidx a GitGame!\n")
 name=input("¿Podrías decirme tu nombre?\n")
@@ -30,13 +27,16 @@ while(unstod!=0):
        
 os.system("cls")
 
-print("\n Ok, " + name + " a todo esto, ¿qué es Git?\n")
+print("\nOk, " + name + " a todo esto, ¿qué es Git?\n")
 print('''Git es un sistema de control de versiones distribuido. Esto significa que un clon local del proyecto es un repositorio de control de versiones completo. Estos repositorios locales plenamente funcionales permiten trabajar sin conexión o de forma remota fácilmente. Los desarrolladores confirman su trabajo localmente y, a continuación, sincronizan su copia del repositorio con la copia en el servidor.''')
 
+time.sleep(10)
+print("\nTenemos una prueba para ti, estás listx, " + name + "?")
+time.sleep(0.5)
 questions_prompts = [
-    "¿Qué es Git? \n(a) Un repositorio web \n(b) Un entorno de desarrollo de Python \n(c) Un sistema de control de versiones\n\n",
-    "¿Dónde se sincroniza el clon del repositorio local? \n(a) En la computadora del vecino \n(b) En los servidores de Git \n(c) En una libreta\n\n",
-    "Como desarrollador, ¿sirve conocer Git? \n(a) Nop \n(b) Para nada \n(c) Ninguna de las anteriores\n\n"
+    "¿Qué es Git? \na: Un repositorio web \nb: Un entorno de desarrollo de Python \nc: Un sistema de control de versiones\n\n",
+    "¿Dónde se sincroniza el clon del repositorio local? \na: En la computadora del vecino \nb: En los servidores de Git \nc: En una libreta\n\n",
+    "Como desarrollador, ¿sirve conocer Git? \na: Nop \nb: Para nada \nc: Ninguna de las anteriores\n\n"
 ]
 
 question_list = [
@@ -72,11 +72,64 @@ if newchance==1:
     run_test(question_list)
 else:
     pass
-        
+time.sleep(0.5)
         
 os.system("cls")
 print("\nSecuencia Introductoria \n")
 print("Hola de nuevo, " + name + ". Este es el primer nivel, donde veremos una breve introducción a la mayoría de los comandos de git \n")
-print('''\n Empecemos con el subnivel 1: Commits de Git.
-      Un commit en un repositorio git registra una captura de todos los archivos en tu directorio. Es como el viejo "copy-paste", pero mejorado;)
-''')
+
+print('''\n Empecemos con el subnivel 1: Commits de Git.\n
+      Un commit en un repositorio git registra una captura de todos los archivos en tu directorio. Es como el viejo 'copy-paste', ¡pero mejorado!\n
+      Git pretende mantener los commits tan ligeros como sea posible, por lo que no copia total e indiscriminadamente el directorio completo cada
+      vez que haces un commit. Puede comprimir un commit como un conjunto de cambios entre una versión de tu repositorio y la siguiente\n
+      Git mantiene un historial de qué commits se hicieron por quién y cuándo. Es por eso que la mayoría de los commits tienen commits precursores arriba suyo.
+      En esta app, indicamos con flechas este flujo para una visualización más sencilla.\n 
+      Hay un montón de cosas por saber, pero por ahora centrémonos en que los commits son 'capturas' de tu proyecto. Son livianos y cambiar de uno a otro es muy rápido.\n''')
+
+
+os.system("cls")
+time.sleep(4)      
+print("¡Excelente, " + name + "! Ya sabes lo que son los commits, ahora comencemos con una pequeña prueba...\n")
+print('''INSTRUCCIONES: Esta prueba es una Trivia, a diferencia de la prueba inicial, aquí si te daremos pistas o la respuesta si fallas tus intentos.\n
+      Escribe la letra de la opción correcta, si necesitas una pista escribe 'P' o 'PISTA'.\n
+      Pero cuidado, sólo tienes 3 pistas por juego. ¡Mucha suerte!\n''')
+
+trivia_com = [
+    {
+        'question': '¿Qué podríamos decir que es un commit?',
+        'options': ["Un algoritmo complicado", "Una instántanea o captura de mi proyecto", "Un apartado donde poner mi nombre de usuario", "Un espacio en el servidor local", "Una plataforma para ver las diferencias entre mi código y el de mi rival"],
+        'answerIndex': 1,
+        'hint': "Es como 'sacarle una foto' a tu trabajo"
+    },
+    {
+        'question': '¿Por qué Git a veces no hace copias idénticas del repositorio?',
+        'options': ["Porque es egoísta", "Porque Microsoft le cobra por cada archivo que se suba", "Porque trata que los commits pesen lo más poquito posible", "Porque el tráfico de datos propicia este tipo de comportamientos", "Porque tienen una API que realiza esto"],
+        'answerIndex': 2,
+        'hint': 'La respuesta es liviana como una pluma...'
+    }
+]
+
+# Puntos
+total_points = 0
+
+# Pistas
+hint = 0
+
+
+#TODO: Agregar un loop para repetir la pregunta cuando se pida una pista.
+for t in trivia_com:
+
+    options = '\n'.join(f'{chr(65 + i)}: {opt}' for i, opt in enumerate(t['options']))
+    answer = input(t['question'] + '\n' + options + "\n\nTu respuesta: ").upper()
+
+    if (ord(answer) - 65) == t['answerIndex']:
+        print("Correcto!:D\n")
+    elif answer == "P" or answer == "PISTA":
+        hint += 1
+        print(f"* {t['hint']}, {3 - hint} pista(s) restante(s). * \n")
+        if hint > 3:
+            print("Lo sentimos, has agotado tus pistas:'(.")
+            continue
+        
+    else:
+        print(f"Oops! Te equivocaste. La respuesta es {chr(65 + t['answerIndex'])}.\n")
